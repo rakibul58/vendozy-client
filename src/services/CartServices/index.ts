871025f6060af4b117361c85842a9b8b4a3a86fc
rakibul58 <rhrahi14@gmail.com>
2024-12-21 20:38:11 +0600

    changes

---
+++ src/services/CartServices/index.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import axiosInstance from "@/lib/AxiosInstance";
+import { FieldValues } from "react-hook-form";
+
+export const addToCart = async (cartData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/carts", cartData);
+    return data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || "Failed to add to cart");
+  }
+};
+
+export const getCart = async () => {
+  try {
+    const { data } = await axiosInstance.get(`/carts`);
+    return data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || "Failed to fetch cart");
+  }
+};
+
+export const clearCart = async () => {
+  try {
+    const { data } = await axiosInstance.delete(`/carts`);
+    return data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || "Failed to clear cart");
+  }
+};
+
+export const removeFromCart = async (cartItemId: string) => {
+  try {
+    const { data } = await axiosInstance.delete(
+      `/carts/cartItem/${cartItemId}`
+    );
+    return data;
+  } catch (error: any) {
+    throw new Error(
+      error.response?.data?.message || "Failed to remove from cart"
+    );
+  }
+};
+
+export const updateCart = async (cartItemId: string, cartData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.put(
+      `/carts/cartItem/${cartItemId}`,
+      cartData
+    );
+    return data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || "Failed to update cart");
+  }
+};
